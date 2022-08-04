ececd3b14a966a7a286a233c68bfa2f1f9ec7637
saoyedotun <oyedotunsodiq045@yahoo.com>
2022-08-04 15:42:39 -0500

    Customers Who Never Order, MySQL, PL/SQL, MSSQL, LeetCode 183

--- customers-who-never-order.sql
+++ customers-who-never-order.sql
@@ -1,6 +1,27 @@
--- MySQL, Oracle PL/SQL, MSSQL
+-- MySQL, MSSQL
+SELECT name AS Customers
+FROM Customers
+         LEFT JOIN Orders
+                   ON Customers.id = Orders.customerId
+WHERE customerId IS NULL
+
 SELECT name AS Customers
 FROM Orders
          RIGHT JOIN Customers
                     ON Orders.customerId  = Customers.id
-WHERE Orders.customerId IS NULL
+WHERE customerId IS NULL
+
+-- Oracle PL/SQL
+SELECT name AS Customers
+FROM Customers
+         LEFT JOIN Orders
+                   ON Customers.id = Orders.customerId
+WHERE customerId IS NULL
+ORDER BY name
+
+SELECT name AS Customers
+FROM Orders
+         RIGHT JOIN Customers
+                    ON Orders.customerId  = Customers.id
+WHERE customerId IS NULL
+ORDER BY name
