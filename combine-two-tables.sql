90f82b221f7ed63d09e101122b9dd60f11ba031b
saoyedotun <oyedotunsodiq045@yahoo.com>
2022-08-04 12:46:54 -0500

    Combine Two Tables, MySQL, PL/SQL, LeetCode 175

--- combine-two-tables.sql
+++ combine-two-tables.sql
@@ -1,19 +1,26 @@
+-- MySQL
 SELECT firstName, lastName, city, state
 FROM Person
-LEFT JOIN Address
-    ON Person.personId = Address.personId
+         LEFT JOIN Address
+                   ON Person.personId = Address.personId
 
-# SELECT firstName, lastName, city, state
-# FROM Person
-# LEFT OUTER JOIN Address
-#     ON Person.personId = Address.personId
+SELECT firstName, lastName, city, state
+FROM Person
+         LEFT OUTER JOIN Address
+                         ON Person.personId = Address.personId
 
-# SELECT firstName, lastName, city, state
-# FROM Address
-# RIGHT JOIN Person
-#     ON Address.personId = Person.personId
+SELECT firstName, lastName, city, state
+FROM Address
+         RIGHT JOIN Person
+                    ON Address.personId = Person.personId
 
-# SELECT firstName, lastName, city, state
-# FROM Address
-# RIGHT OUTER JOIN Person
-#     ON Address.personId = Person.personId
+SELECT firstName, lastName, city, state
+FROM Address
+         RIGHT OUTER JOIN Person
+                          ON Address.personId = Person.personId
+
+-- Oracle PL/SQL
+SELECT firstName, lastName, city, state
+FROM Person
+         LEFT JOIN Address
+                   ON Person.personId = Address.personId
