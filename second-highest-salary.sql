156835eb7a2a8e87420c52e7ad852094c269103a
saoyedotun <oyedotunsodiq045@yahoo.com>
2022-08-04 11:14:56 -0500

    Second Highest Salary, MySQL, PL/SQL, LeetCode 176

--- second-highest-salary.sql
+++ second-highest-salary.sql
@@ -3,3 +3,9 @@ FROM Employee
 WHERE salary NOT IN
       (SELECT MAX(salary)
        FROM Employee)
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary <
+      (SELECT MAX(salary)
+       FROM Employee)
