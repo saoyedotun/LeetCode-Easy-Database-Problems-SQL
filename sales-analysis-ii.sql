596d405948cb4d1019521f567a617e4875eed3ab
saoyedotun <oyedotunsodiq045@yahoo.com>
2022-09-13 19:54:21 -0500

    Sales Analysis II, MySQL, PL/SQL, MSSQL, LeetCode 1083

--- sales-analysis-ii.sql
+++ sales-analysis-ii.sql
@@ -4,5 +4,6 @@ FROM Product
 JOIN Sales
     ON Product.product_id = Sales.product_id
 GROUP BY buyer_id
+-- HAVING SUM(CASE WHEN product_name = "S8" THEN 1 ELSE 0 END) > 0
 HAVING SUM(product_name = "S8") > 0
     AND SUM(product_name = "iphone") = 0
